fdd8219c2b79cb96e818fb999eb88c2fb911859b
PharrellWANG <15113029g@connect.polyu.hk>
2017-02-04 17:08:50 +0800

    crimemapinfo migrated to cmdata

--- hkcm/models.py
+++ hkcm/models.py
@@ -1,5 +1,52 @@
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
+@python_2_unicode_compatible
+class Cmdata(models.Model):
+    issuetime = models.DateTimeField(null=True)
+    location = models.CharField(max_length=50)
+    crime = models.CharField(max_length=50)
+    crimecat = models.CharField(max_length=50)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+    title = models.CharField(max_length=500)
+    URL = models.CharField(max_length=5000)
+
+    class Meta:
+        unique_together = ('title', 'location', 'crime')
+
+    def __str__(self):
+        return self.crime
+
+    def was_published_recently(self):
+        return self.issuetime >= timezone.now() - datetime.timedelta(days=3)
+
+
+@python_2_unicode_compatible
+class Crimemapinfo(models.Model):
+    # id = models.AutoField()
+    issuetime = models.CharField(max_length=50, blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    crime = models.CharField(max_length=50, blank=True, null=True)
+    crimecat = models.CharField(max_length=50, blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    title = models.CharField(max_length=50)
+    url = models.CharField(db_column='URL', max_length=500, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'crimemapinfo'
+        unique_together = (('id', 'title'), ('issuetime', 'location', 'crime'),)
+
+    def __str__(self):
+        return self.crime
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
--- proj_hkcm/settings.py
+++ proj_hkcm/settings.py
@@ -14,7 +14,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -26,10 +25,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'hkcm.apps.HkcmConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'proj_hkcm.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
@@ -84,6 +82,10 @@ DATABASES = {
     }
 }
 
+DATABASE_OPTIONS = {
+    "charset": "utf8mb4",
+    # "init_command": "SET storage_engine=InnoDB",
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -103,20 +105,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Hong_Kong'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
