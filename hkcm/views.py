e889edb1e54d960afa3b4646380ad30cfae8bad6
PharrellWANG <15113029g@connect.polyu.hk>
2017-02-05 23:05:19 +0800

    Successfully migrated to Django and Metronic.

--- hkcm/views.py
+++ hkcm/views.py
@@ -1,98 +1,70 @@
 # This Python file uses the following encoding: utf-8
-from django.http import HttpResponse
+import json
+import logging
+from collections import OrderedDict
+
 from django.shortcuts import render
 
 from .models import Cmdata
 
-import logging
 log = logging.getLogger(__name__)
 log.debug("")
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the hong kong crime map index.")
+def filters(request):
+    content = {
+    }
+
+    return render(request, 'filter.html', content)
 
 
 def map_page(request):
-    a1 = []
-    a2 = []
-    a3 = []
-    a4 = []
-    a5 = []
-    a6 = []
-    a7 = []
+    outer_dic = OrderedDict()
+    all_records = Cmdata.objects.all()
+    auto_id = 0
+    for entry in all_records:
+        # log.debug("====")
+        # log.debug(auto_id)
+        # log.debug(entry)
 
-    latitude = Cmdata.objects.values_list('latitude')
-    for x in latitude:
-        x = x[0]
-        a1.append(x)
-    log.debug(type(a1[1]))
+        inner_dic = OrderedDict()
 
-    longitude = Cmdata.objects.values_list('longitude')
-    for x in longitude:
-        x = x[0]
-        a2.append(x)
+        it = entry.issuetime
+        it.strftime('%Y-%m-%d %H:%M')
+        it = str(it)[0:16]
+        inner_dic["id"] = auto_id
+        inner_dic["issue_time"] = it
+        inner_dic["location"] = entry.location
+        inner_dic["crime"] = entry.crime
+        inner_dic["crimecat"] = entry.crimecat
+        inner_dic["latitude"] = entry.latitude
+        inner_dic["longitude"] = entry.longitude
+        inner_dic["title"] = entry.title
+        inner_dic["URL"] = entry.URL
+        outer_dic[auto_id] = inner_dic
+        auto_id += 1
+    length_of_records = auto_id
+    log.debug(auto_id)
+    outer_dic = json.dumps(outer_dic)
 
-    issue_time = Cmdata.objects.values_list('issuetime')
-    for x in issue_time:
-        x = x[0]
-        x.strftime('%Y-%m-%d %H:%M')
-        # print x
-        a3.append(x)
+    crimecat_selector = [{'name': "總體罪案(All)", 'value': 0},
+                         {'name': "劫案(Robbery)", 'value': 1},
+                         {'name': "暴力罪案(Violent Crime)", 'value': 2},
+                         {'name': "爆竊案(Burglary)", 'value': 3},
+                         {'name': "傷人及嚴重毆打(Wounding and Serious Assault)", 'value': 4},
+                         {'name': "刑事恐嚇(Criminal Intimidation)", 'value': 5},
+                         {'name': "強姦及非禮(Rape)", 'value': 6},
+                         {'name': "嚴重毒品罪行(Serious Drug Offenses)", 'value': 7}]
 
-    valid_a4 = []
-    crime = Cmdata.objects.values_list('crime')
-    for x in crime:
-        x = x[0]
-        a4.append(x)
-    length_of_a4 = len(a4)
-    for i in range(length_of_a4):
-        x = a4[i].encode('utf-8')
-        valid_a4.append(x)
-    a4 = valid_a4
-    log.debug(type(a4))
-
-    log.debug(a4[3])
-    log.debug(type(a4[3]))
-    z = a4[3]
-    # z = '"' + z + '"'
-    # x = "你好"
-    # log.debug(type(x))
-
-    location = Cmdata.objects.values_list('location')
-    for x in location:
-        x = x[0]
-        a5.append(x)
-
-    title = Cmdata.objects.values_list('title')
-    for x in title:
-        x = x[0]
-        a6.append(x)
-
-    url = Cmdata.objects.values_list('URL')
-    for x in url:
-        x = x[0]
-        a7.append(x)
-    log.debug(a7[3])
-    log.debug(type(a7[3]))
-
-    length_of_a1 = len(a1)
+    # crimecat_selector.append({'name': "總體罪案", 'value': "All"})
+    # crimecat_selector.append({'name': "總體罪案", 'value': "All"})
+    # crimecat_selector.append({'name': "總體罪案", 'value': "All"})
+    # crimecat_selector.append({'name': "總體罪案", 'value': "All"})
 
     content = {
-        'a1': a1,
-        'a2': a2,
-        'a3': a3,
-        'a4': a4,
-        'a5': a5,
-        'a6': a6,
-        'a7': a7,
-        'length_a1': length_of_a1,
-        'z': z,
-        # 'length_a2': length_of_a2,
-        # 'length_a3':length_of_a3,
-        # 'length_a4': length_of_a4,
-        # 'length_a3': length_of_a3,
-
+        'outer_dic': outer_dic,
+        'le': length_of_records,
+        'crimecat_selector': crimecat_selector,
     }
 
     return render(request, 'index.html', content)
