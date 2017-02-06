1ce909833fc2a053c235a35944fbb7cfc32fa8e4
PharrellWANG <15113029g@connect.polyu.hk>
2017-02-06 15:22:43 +0800

    url dispatcher changed

--- proj_hkcm/urls.py
+++ proj_hkcm/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hkcm/', include('hkcm.urls')),
+    url(r'^', include('hkcm.urls', namespace="hkcm")),
 ]
 
 # urlpatterns += patterns('',
