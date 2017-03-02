aa40cc07e099404069f4cadc2842c065eb8a956b
PharrellWANG <15113029g@connect.polyu.hk>
2017-03-02 11:02:18 +0800

    many changes

--- hkcm/models.py
+++ hkcm/models.py
@@ -59,3 +59,14 @@ class HongKongEighteenDistricts(models.Model):
     def __str__(self):
         return self.Districts
 
+@python_2_unicode_compatible
+class latlngMappingDistricts(models.Model):
+    id_in_d = models.FloatField(null=True)
+    # location = models.CharField(max_length=200, unique=True)
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+    Districts = models.CharField(max_length=200)
+    # Districts_chn = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.location
